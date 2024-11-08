cd4c73e350fa08ce902b265ec6b4f0a303f3646f
Dan Abramov <dan.abramov@gmail.com>
2024-11-08 18:26:16 +0000

    withDecay bug

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -3,50 +3,36 @@ import {Pressable, Text} from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
+  withDecay,
 } from 'react-native-reanimated';
 
 export default function App() {
-  const [show, setShow] = useState(false);
-  return show ? (
-    <Lightbox onClose={() => setShow(false)} />
-  ) : (
-    <Pressable
-      style={{padding: 100, fontSize: 50, backgroundColor: 'purple'}}
-      onPress={() => setShow(true)}>
-      <Text>open</Text>
-    </Pressable>
-  );
-}
-
-function Lightbox({onClose}) {
-  const openProgress = useSharedValue(0);
-
-  React.useEffect(() => {
-    console.log('set to 1');
-    openProgress.value = 1;
-  }, [openProgress]);
-
-  const containerStyle = useAnimatedStyle(() => {
-    console.log(openProgress.value);
-    if (openProgress.value < 1) {
-      return {backgroundColor: 'red'};
-    }
-    return {backgroundColor: 'blue'};
-  });
-
+  const translateY = useSharedValue(0);
+  const style = useAnimatedStyle(() => ({
+    transform: [{translateY: translateY.value}],
+  }));
   return (
-    <Animated.View
-      style={[
-        {
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        },
-        containerStyle,
-      ]}>
-      <Pressable style={{flex: 1}} onPress={onClose} />
-    </Animated.View>
+    <>
+      <Pressable
+        style={{padding: 100, fontSize: 50, backgroundColor: 'purple'}}
+        onPress={() => {
+          translateY.value = withDecay({
+            velocity: 200,
+            velocityFactor: 1,
+          });
+        }}>
+        <Text>open</Text>
+      </Pressable>
+      <Animated.View
+        style={[
+          {
+            backgroundColor: 'blue',
+            width: 100,
+            height: 100,
+          },
+          style,
+        ]}
+      />
+    </>
   );
 }
